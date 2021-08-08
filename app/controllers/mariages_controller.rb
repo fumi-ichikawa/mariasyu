d3a4e337b1a7e69cb705e98ed911dfd47d178e5f
fumi-ichikawa <japanesequeen1114@gmail.com>
2021-08-08 15:48:16 +0900

    new,createアクションの定義

--- app/controllers/mariages_controller.rb
+++ app/controllers/mariages_controller.rb
@@ -2,6 +2,14 @@ class MariagesController < ApplicationController
   def index
   end
 
+  def new
+    @mariage = Mariage.new
+  end
+
+  def create
+  end
+
+
   private
 
   def mariage_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
  root to: "mariages#index"
+ resources :mariages,only: [:index, :new, :create]
 end
