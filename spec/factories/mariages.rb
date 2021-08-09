7b77139cabbe1c3918269c58441679d586344e46
fumi-ichikawa <japanesequeen1114@gmail.com>
2021-08-09 17:35:55 +0900

    mariageモデル単体テストの実施

--- spec/factories/mariages.rb
+++ spec/factories/mariages.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :mariage do
+    title { Faker::Lorem.sentence }
+    text { Faker::Lorem.sentence }
+    category_id { Faker::Number.between(from: 2, to: 6) }
+    taste_id { Faker::Number.between(from: 2, to: 6) }
     
+    association :user
+
+    after(:build) do |mariage|
+      mariage.image.attach(io: File.open('public/images/test_image.jpg'), filename: 'test_image.jpg')
+    end
   end
 end
