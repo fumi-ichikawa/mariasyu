6dc99b0ac78b42d4117f830fc1ab3afd3e624ac2
fumi-ichikawa <japanesequeen1114@gmail.com>
2021-08-10 12:15:43 +0900

    アクションとルーティングの設定

--- app/controllers/mariages_controller.rb
+++ app/controllers/mariages_controller.rb
@@ -20,6 +20,19 @@ class MariagesController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+  
+
   private
 
   def mariage_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
  root to: "mariages#index"
- resources :mariages,only: [:index, :new, :create]
+ resources :mariages
 end
