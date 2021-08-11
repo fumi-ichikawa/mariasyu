30c9a7e1e6ae69d5e3702cc44671c6d5f000568e
fumi-ichikawa <japanesequeen1114@gmail.com>
2021-08-11 15:49:25 +0900

    commentsコントローラーの作成

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,17 @@
+class CommentsController < ApplicationController
+  def create
+    @comment = Comment.create(comment_params)
+    if @comment.save
+      redirect_to mariage_path(@comment.mariage)
+    else
+      @mariage = @comment.mariage
+      @comments = @mariage.comments
+      render "mariage/show" 
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, mariage_id: params[:mariage_id])
+  end
+end
