35128ec37e63df89eba57f9f34079913120fbc9e
fumi-ichikawa <japanesequeen1114@gmail.com>
2021-08-11 15:49:42 +0900

    ルーティングのネスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
- root to: "mariages#index"
- resources :mariages
+  root to: "mariages#index"
+  resources :mariages do
+    resources :comments, only: :create
+  end
 end
