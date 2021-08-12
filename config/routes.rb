9bde39155b1508af48ef01acd84794cabe699603
fumi-ichikawa <japanesequeen1114@gmail.com>
2021-08-12 12:20:03 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "mariages#index"
+  resources :users, only: :show
   resources :mariages do
     resources :comments, only: :create
   end
