f562a1bec02dd860bf4db3c50eaa0623e60c2940
fumi-ichikawa <japanesequeen1114@gmail.com>
2021-08-19 16:52:01 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'users/registrations'
   }
   root to: "mariages#index"
+  get 'searches/index', to: 'searches#index'
+  get 'searches/search', to: 'searches#search'
   resources :users, only: :show
   resources :mariages do
     resources :comments, only: :create
