6d32f6a80b8a72f75af7c0669cdfc55f91b9e14a
fumi-ichikawa <japanesequeen1114@gmail.com>
2021-08-20 21:56:48 +0900

    テストコード実施時のBasic認証対策

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :basic_auth
+  before_action :basic_auth if Rails.env.production?
   before_action :search_mariage
 
   private
@@ -11,11 +11,12 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']  # 環境変数を読み込む記述に変更
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
     end
   end
 
   def search_mariage
     @p = Mariage.ransack(params[:q])
   end
+
 end
