8df3d4bcdcaf13865ae8d493349ed2d66eb165e6
fumi-ichikawa <japanesequeen1114@gmail.com>
2021-08-26 20:49:22 +0900

    JavaScriptの記述

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,31 +1,41 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function() {
   if ( document.getElementById('mariage_image')){
-
-    const ImageList = document.getElementById('image-list');
+    const ImageList = document.getElementById('image-list')
 
     const createImageHTML = (blob) => {
        // 画像を表示するためのdiv要素を生成
-      const imageElement = document.createElement('div');
+      const imageElement = document.createElement('div')
+      imageElement.setAttribute('class', "image-element")
+      let imageElementNum = document.querySelectorAll('.image-element').length
 
       // 表示する画像を生成
-      const blobImage = document.createElement('img');
-      blobImage.setAttribute('src', blob);
+      const blobImage = document.createElement('img')
+      blobImage.setAttribute('src', blob)
+
+      // ファイル選択ボタンを生成
+      const inputHTML = document.createElement('input')
+      inputHTML.setAttribute('id', `mariage_image_${imageElementNum}`)
+      inputHTML.setAttribute('name', 'mariage[images][]')
+      inputHTML.setAttribute('type', 'file')
 
       // 生成したHTMLの要素をブラウザに表示させる
-      imageElement.appendChild(blobImage);
-      ImageList.appendChild(imageElement);
-    };
+      imageElement.appendChild(blobImage)
+      imageElement.appendChild(inputHTML)
+      ImageList.appendChild(imageElement)
 
-    document.getElementById('mariage_image').addEventListener('change', function(e){
-      const imageContent = document.querySelector('img');
-      if (imageContent){
-        imageContent.remove();
-      }
+      inputHTML.addEventListener('change', (e) => {
+        file = e.target.files[0];
+        blob = window.URL.createObjectURL(file);
 
-      const file = e.target.files[0];
-      const blob = window.URL.createObjectURL(file);
+        createImageHTML(blob)
+      })
+    }
+    document.getElementById('mariage_image').addEventListener('change', (e) => {
+      let file = e.target.files[0];
+      let blob = window.URL.createObjectURL(file);
 
-      createImageHTML(blob);
+      createImageHTML(blob)
+
     });
   }
-});
+})
