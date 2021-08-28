40232b3f3417a0387b2a4bd9b5200ab5cbfdf457
fumi-ichikawa <japanesequeen1114@gmail.com>
2021-08-28 19:01:29 +0900

    画像データの上書き→追加に変更

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,6 @@ module Mariasyu
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.time_zone = 'Tokyo'
+    config.active_storage.replace_on_assign_to_many = false
   end
 end
