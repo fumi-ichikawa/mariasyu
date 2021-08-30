7cda39d4df4714041166e30dc7853717ceb16c77
fumi-ichikawa <japanesequeen1114@gmail.com>
2021-08-31 02:28:21 +0900

    アクションの定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,28 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
   def show
-    @user = User.find(params[:id])
     @mariages = @user.mariages.includes(:user)
+  end
+
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to "/users/#{current_user.id}"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:nickname, :email)
   end
 end
